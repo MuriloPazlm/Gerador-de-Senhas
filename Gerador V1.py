af5ae743800cfc3472209d08677c3745e3302cd2
Murilo Paz <murilolima3200@gmail.com>
2024-11-06 15:25:05 -0300

    Adição de proteção entrada na opcão "gerar senha" com o uso de while, tamanho da senha ajustavél em até 10 caracteres.

--- Gerador V1.py
+++ Gerador V1.py
@@ -2,16 +2,21 @@ from random import randint
 
 senha = [(randint(0,10)),(randint(0,10)),(randint(0,10)),
          (randint(0,10)),(randint(0,10)),(randint(0,10)),
+         (randint(0,10)),(randint(0,10)),(randint(0,10)),
          (randint(0,10))]
+v1= 0
 
 print('-='*20)
 print('Seja bem vindo ao seu gerador de senhas!')
 print('-='*20)
 
 v1 = str(input('Deseja gerar uma senha agora? (S/N)  ')).upper()
+while v1 not in "SsNn":
+    print('Por favor digite apenas S ou N!')
+    v1 = str(input('Deseja gerar uma senha agora? (S/N)  ')).upper()
+
 if v1 in "Ss":
     print('Que ótimo!')
-    v2 = int(input('Quantos caracteres deseja? '))
-    
-    print(f'Aqui está sua senha gerada:')
-    print("".join(map(str, senha)))
+    v2 = int(input('Quantos caracteres de 1 a 10 deseja? '))
+    print('Aqui está sua senha gerada:')
+    print(f"".join(map(str, senha[0:v2])))
